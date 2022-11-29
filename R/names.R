93ab42dde69ecad9d89d6f717bbcf64f5d2c2c7f
Antoine Fabri <antoine.fabri@gmail.com>
2022-11-29 11:10:33 +0100

    Implement length methods

--- R/names.R
+++ R/names.R
@@ -19,3 +19,23 @@ names.ev_package <- function(x, pattern="") {
   pkg <- attr(x, "name")
   fetch_evs(pkg)
 }
+
+#' @export
+length.opt_object <- function(x) {
+  length(names(x))
+}
+
+#' @export
+length.opt_package <- function(x) {
+  length(names(x))
+}
+
+#' @export
+length.ev_object <- function(x) {
+  length(names(x))
+}
+
+#' @export
+length.ev_package <- function(x) {
+  length(names(x))
+}
